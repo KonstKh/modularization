e8d1a6b3da63e9f4795f5d0b2246c2faa8a33722
KonstKh <const.kharchenko@gmail.com>
2020-03-29 11:30:25 +0200

    travis added

--- src/api/deal/deal.controller.ts
+++ src/api/deal/deal.controller.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
 import { Logger } from 'winston';
-import DealService from './deal.service';
-
 import { decorateRouter } from '@awaitjs/express';
+
+import DealService from './deal.service';
 import { IDealDTO } from './deal.interfaces';
 
 const router = decorateRouter(Router());
@@ -13,7 +13,7 @@ export default (app: Router) => {
 
   router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     const logger: Logger = Container.get('logger');
-    logger.debug('Get deal');
+    logger.debug('Create deal');
     try {
       const dealService = Container.get(DealService);
       const deal = await dealService.CreateDeal(req.body as IDealDTO);
